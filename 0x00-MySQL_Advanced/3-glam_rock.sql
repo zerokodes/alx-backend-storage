5316bee48a1a87e965dbfbe47357456aa423691c
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-13 19:01:44 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,6 @@
--- Lists all bands with Glam rock as their style,
--- ranked by their longevity
--- Column names must be: band_name & lifespan
-
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+-- List all bands with Glam rock
+SELECT band_name,
+	IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
