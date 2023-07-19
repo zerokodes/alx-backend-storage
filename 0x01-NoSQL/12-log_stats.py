d0c071afb7a91aac101502c4dca0321fc3c88776
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-19 04:29:06 +0100

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
+"""  provides stats about nginx"""
 
-'''A Python module tha provides stats about nginx'''
+if __name__ == "__main__":
+    from pymongo import MongoClient
 
-from pymongo import MongoClient
+    def log_stats():
+        """provides stats about Nginx logs stored in MongoDB"""
+        # Connect databse
+        client = MongoClient()
+        db = client["logs"]
+        col = db["nginx"]
+
+        methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+        # Print output given in example
+        print(col.count_documents({}), "logs")
+
+        print("Methods:")
+        for method in methods:
+            logs = col.count_documents({"method": method})
+            print("\tmethod {}: {}".format(method, logs))
+
+        print(col.count_documents(
+            {"method": "GET", "path": "/status"}), "status check")
 
 
-if __name__ == '__main__':
-    '''Prints the log stats in nginx collection'''
-    con = MongoClient('mongodb://localhost:27017')
-    collection = con.logs.nginx
-
-    print(f'{collection.estimated_document_count()} logs')
-
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    print('Methods:')
-
-    for req in methods:
-        print('\tmethods {}: {}'.format(req,
-              collection.count_documents({'method': req})))
-
-    print('{} status check'.format(collection.count_documents(
-          {'method': 'GET', 'path': '/status'})))
+# call main function
+    log_stats()
