2d71b19070420ac0ba2ca4aff32cc930fd86986d
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-20 04:19:39 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,11 +6,18 @@ import requests
 
 
 redi = redis.Redis()
-count = 1
+count = 0
 
 
 def get_page(url: str) -> str:
     """ Impelements core function of get page which is accessed."""
+     """ Check if the URL's content is already cached in Redis.""""
+    cached_content = redi.get(url)
+    if cached_content:
+        """" Increment the access count for this URL """
+        redi.incr(f"count:{url}")
+        return cached_content.decode('utf-8')
+        
     redi.set(f"cached:{url}", count)
     response = requests.get(url)
     redi.incr(f"count:{url}")
