4cce911e3c32d96b8328a27edd74e491557afbd9
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-20 03:35:23 +0100

    fixed advance task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,36 +2,36 @@
 """ Implementing an expiring web cache and tracker
     obtain the HTML content of a particular URL and returns it """
 import requests
+import redis
 import time
-import functools
 
-# A dictionary to store the cached results and access counts
-cache = {}
+redis_client = redis.Redis()
 
 def expiration_time():
     return int(time.time()) + 10  # Set the cache expiration time to 10 seconds
 
-def cache_decorator(func):
-    @functools.wraps(func)
-    def wrapper(url):
-        key = f"count:{url}"
-        # Check if the URL is already cached
-        if url in cache and cache[url]["expiration"] > time.time():
-            cache[key]["count"] += 1
-            return cache[url]["content"]
-        # If not cached or expired, fetch the content and cache it
-        response = requests.get(url)
-        content = response.text
-        cache[url] = {"content": content, "expiration": expiration_time()}
-        # Initialize the access count if not present
-        if key not in cache:
-            cache[key] = {"count": 1}
-        else:
-            cache[key]["count"] += 1
-        return content
-    return wrapper
-
-@cache_decorator
 def get_page(url: str) -> str:
+    key = f"count:{url}"
+    
+    # Check if the URL's content is already cached in Redis
+    cached_content = redis_client.get(url)
+    if cached_content:
+        # Increment the access count for this URL
+        redis_client.incr(key)
+        return cached_content.decode('utf-8')
+    
+    # If not cached, fetch the content using requests
     response = requests.get(url)
-    return response.text
+    content = response.text
+    
+    # Cache the content in Redis with an expiration time
+    redis_client.setex(url, expiration_time(), content)
+    
+    # Initialize the access count if not present
+    if not redis_client.exists(key):
+        redis_client.set(key, 1)
+    else:
+        # Increment the access count for this URL
+        redis_client.incr(key)
+    
+    return content
