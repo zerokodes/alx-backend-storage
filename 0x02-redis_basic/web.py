ea98ac230837eaffdf4e630d93f438969110638c
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-20 04:08:02 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,33 +5,35 @@ import requests
 import redis
 import time
 
-redis_client = redis.Redis()
+""" Connect to Redis (make sure Redis server is running) """
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def expiration_time():
-    return int(time.time()) + 10  # Set the cache expiration time to 10 seconds
+    return int(time.time()) + 10  """ Set the cache expiration time to 10 seconds """
 
 def get_page(url: str) -> str:
     key = f"count:{url}"
     
-    # Check if the URL's content is already cached in Redis
+    """ Check if the URL's content is already cached in Redis """
     cached_content = redis_client.get(url)
     if cached_content:
-        # Increment the access count for this URL
+        """ Increment the access count for this URL """
         redis_client.incr(key)
         return cached_content.decode('utf-8')
     
-    # If not cached, fetch the content using requests
+    """ If not cached, fetch the content using requests """
     response = requests.get(url)
     content = response.text
     
-    # Cache the content in Redis with an expiration time
-    redis_client.setex(f"cached:{url}", expiration_time(), redis_client.get(f"cached:{url}"))
+    """ Cache the content in Redis with an expiration time """
+    redis_client.setex(url, expiration_time(), content)
     
-    # Initialize the access count if not present
+    """ Initialize the access count if not present """
     if not redis_client.exists(key):
-        redis_client.set(f"cached:{url}", 1)
+        redis_client.set(key, 1)
     else:
-        # Increment the access count for this URL
+        """ Increment the access count for this URL """
         redis_client.incr(key)
     
     return content
+
