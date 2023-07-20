cd60dd4d7e821635227d4406e613ac3626c142f3
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-20 03:19:32 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python3
 """ Implementing an expiring web cache and tracker
     obtain the HTML content of a particular URL and returns it """
-import redis
 import requests
+import time
+import functools
 
+# A dictionary to store the cached results and access counts
+cache = {}
 
-redi = redis.Redis()
-count = 0
+def expiration_time():
+    return int(time.time()) + 10  # Set the cache expiration time to 10 seconds
 
+def cache_decorator(func):
+    @functools.wraps(func)
+    def wrapper(url):
+        key = f"count:{url}"
+        # Check if the URL is already cached
+        if url in cache and cache[url]["expiration"] > time.time():
+            cache[key]["count"] += 1
+            return cache[url]["content"]
+        # If not cached or expired, fetch the content and cache it
+        response = requests.get(url)
+        content = response.text
+        cache[url] = {"content": content, "expiration": expiration_time()}
+        # Initialize the access count if not present
+        if key not in cache:
+            cache[key] = {"count": 1}
+        else:
+            cache[key]["count"] += 1
+        return content
+    return wrapper
 
+@cache_decorator
 def get_page(url: str) -> str:
-    """ Impelements core function of get page which is accessed."""
-    redi.set(f"cached:{url}", count)
     response = requests.get(url)
-    redi.incr(f"count:{url}")
-    redi.setex(f"cached:{url}", 10, redi.get(f"cached:{url}"))
     return response.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
