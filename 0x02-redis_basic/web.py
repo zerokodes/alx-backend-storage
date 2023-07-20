2b96f79986147d068ad9e2c61745d261f9fc28ec
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-20 04:13:09 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,22 @@
 #!/usr/bin/env python3
 """ Implementing an expiring web cache and tracker
     obtain the HTML content of a particular URL and returns it """
-import requests
 import redis
-import time
+import requests
 
-redis_cleint = redis.Redis()
 
-def expiration_time():
-    return int(time.time()) + 10  """ Set the cache expiration time to 10 seconds """
+redi = redis.Redis()
+count = 1
+
 
 def get_page(url: str) -> str:
-    key = f"count:{url}"
-    
-    """ Check if the URL's content is already cached in Redis """
-    cached_content = redis_client.get(url)
-    if cached_content:
-        """ Increment the access count for this URL """
-        redis_client.incr(key)
-        return cached_content.decode('utf-8')
-    
-    """ If not cached, fetch the content using requests """
+    """ Impelements core function of get page which is accessed."""
+    redi.set(f"cached:{url}", count)
     response = requests.get(url)
-    content = response.text
-    
-    """ Cache the content in Redis with an expiration time """
-    redis_client.setex(url, expiration_time(), content)
-    
-    """ Initialize the access count if not present """
-    if not redis_client.exists(key):
-        redis_client.set(key, 1)
-    else:
-        """ Increment the access count for this URL """
-        redis_client.incr(key)
-    
-    return content
+    redi.incr(f"count:{url}")
+    redi.setex(f"cached:{url}", 10, redi.get(f"cached:{url}"))
+    return response.text
 
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
